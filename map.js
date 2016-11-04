59a96755a968dee77bc9766aeb811fd440034d31
Bartosz Holowko <holek10@users.noreply.github.com>
2016-11-04 13:00:01 +0100

    Leaflet map
    Cleaning and refactoring

--- map.js
+++ map.js
@@ -1,15 +1,10 @@
- var map = L.map('map').setView([51.099, 17.026], 13);
+var map = L.map('map').setView([51.099, 17.026], 13);
                       
-      //    L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v9/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiaG9sZWsiLCJhIjoiY2l0dG81ajB1MDAwNDJvcW14bWxsdWN3diJ9.Pe6D3DGCxZfZV7ZQp_-CTA').addTo(map);
-           
 L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v9/tiles/256/{z}/{x}/{y}?access_token={accessToken}', {
-    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors,  <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
-                      maxZoom: 18,
-                    //  id: 'your.mapbox.project.id',
-                      accessToken: 'pk.eyJ1IjoiaG9sZWsiLCJhIjoiY2l0dG81ajB1MDAwNDJvcW14bWxsdWN3diJ9.Pe6D3DGCxZfZV7ZQp_-CTA'
-                      }).addTo(map);
+	attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors,  <a 
+href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+	maxZoom: 18,
+	accessToken: 'pk.eyJ1IjoiaG9sZWsiLCJhIjoiY2l0dG81ajB1MDAwNDJvcW14bWxsdWN3diJ9.Pe6D3DGCxZfZV7ZQp_-CTA'
+}).addTo(map);
 
-           
-          L.marker([51.099, 17.026]).addTo(map)
-            .bindPopup('Unit4 Headquarter (Globis)')
-                .openPopup();
+L.marker([51.099, 17.026]).addTo(map).bindPopup('Company Headquarter').openPopup();
